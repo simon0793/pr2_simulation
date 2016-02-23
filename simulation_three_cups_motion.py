234c4ebd6c8faa568a7b11e30c45c8afd83b6032
Simon <simon0793@github.com>
2016-02-23 14:26:13 -0800

    Working one cup simulation using trajopt and my queue planner

--- simulation_three_cups_motion.py
+++ simulation_three_cups_motion.py
@@ -1,5 +1,5 @@
 import arm
-# import planner
+import planner
 
 import numpy as np
 
@@ -27,9 +27,9 @@ BLUE = [0.701716, 0.000784, 0.83095]
 GREEN = [0.701716, -0.400784, 0.83095]
 RIGHT_UP = [0.5, -0.2, 1.25]
 LEFT_UP = [0.5, 0.2, 1.25]
-TARGET_GREEN = [0.6, -0.405, 0.75]
-GRASP_GREEN = [0.7, -0.405, 0.75]
-DEST_GREEN = [0.7, 0.0, 0.75]
+TARGET_GREEN = [0.6, -0.405, 0.77]
+GRASP_GREEN = [0.7, -0.405, 0.77]
+DEST_GREEN = [0.7, 0.0, 0.79]
 
 
 class SimpleArmState:
@@ -42,7 +42,7 @@ class SimpleArmState:
     self.leftArm = arm.Arm('left')
     self.rightArm.sim.env.SetViewer('qtcoin')
     self.valid = True
-    # graspPlanner = planner.Planner()
+    self.graspPlanner = planner.Planner('right')
     # self.leftArm.sim.env.SetViewer('qtcoin')
 
   def isGoalState(self):
@@ -68,8 +68,8 @@ class SimpleArmState:
     ''' ------ CHANGE CODE ABOVE THIS POINT ------ '''
 
 
-  def succesorState(self):
-    ''' Updates current state to the next succesor state
+  def successorState(self):
+    ''' Updates current state to the next successor state
       by default it just returns current state
     '''
     rightArmPose = self.rightArm.get_pose()
@@ -82,7 +82,7 @@ class SimpleArmState:
       x1, y1, z1 = TARGET_GREEN
       x2, y2, z2 = GRASP_GREEN
       currentX, currentY, currentZ = rightArmPose.position.array
-      acceptableAbsError = 0.05
+      acceptableAbsError = 0.1
       if abs(x1 - currentX) <= acceptableAbsError and abs(y1 - currentY) <= acceptableAbsError and abs(z1 - currentZ) <= acceptableAbsError:
         newRightArmPose.position = GRASP_GREEN
       elif abs(x2 - currentX) <= acceptableAbsError and abs(y2 - currentY) <= acceptableAbsError and abs(z2 - currentZ) <= acceptableAbsError:
@@ -92,10 +92,13 @@ class SimpleArmState:
         self.raiseArms()
         newRightArmPose.position = DEST_GREEN
       else:
+        print "ERROR1: " + str([abs(x1 - currentX), abs(y1 - currentY), abs(z1 - currentZ)])
+        print "ERROR2: " + str([abs(x2 - currentX), abs(y2 - currentY), abs(z2 - currentZ)])
+        rospy.sleep(1)
         newRightArmPose.position = TARGET_GREEN # either update by adding to xyz offset array "+ [0, -pos_step, 0]" or assign new position, defualt keeps position the same
 
     else:
-      print "The last succesor state was invalid"
+      print "The last successor state was invalid"
       print "Enter a custom pose x y z"
       coord = raw_input().split()
       newRightArmPose.position = [float(x) for x in coord]
@@ -104,25 +107,40 @@ class SimpleArmState:
 
     ''' ------ CHANGE CODE ABOVE THIS POINT ------ '''
 
-    newRightArmPose = tfx.pose(newRightArmPose.position, tfx.tb_angles(0.0, 0.0, 0.0))
-    newRightJoints = self.rightArm.ik(newRightArmPose) # Updates arm pose if valid
-    newLeftJoints = self.leftArm.ik(newLeftArmPose)
-    if newRightJoints is not None:
-      self.rightArm.go_to_joints(newRightJoints)
-      print('new_pose: {0}'.format(newRightArmPose))
-      self.valid = True
-    else:
-      rospy.loginfo('Invalid Right Arm Pose')
-      print newRightArmPose
-      self.valid = False
-      return self
+    # newRightArmPose = tfx.pose(newRightArmPose.position, tfx.tb_angles(0.0, 0.0, 0.0))
+    newRightJointsList = self.graspPlanner.get_grasp_joint_trajectory(self.rightArm.get_joints(), newRightArmPose.position, n_steps=10)
+    print newRightJointsList
+    self.rightArm.execute_joint_trajectory(newRightJointsList)
+    # for newRightJoints in newRightJointsList:
+    #   if newRightJoints is not None:
+    #     self.rightArm.go_to_joints(newRightJoints)
+    #     print('new_pose: {0}'.format(newRightArmPose))
+    #     self.valid = True
+    #   else:
+    #     rospy.loginfo('Invalid Right Arm Pose')
+    #     print newRightArmPose
+    #     self.valid = False
+    #     return self
+    #   rospy.sleep(1)
+
+    # newRightJoints = self.rightArm.ik(newRightArmPose) # Updates arm pose if valid
+    # newLeftJoints = self.leftArm.ik(newLeftArmPose)
+    # if newRightJoints is not None:
+    #   self.rightArm.go_to_joints(newRightJoints)
+    #   print('new_pose: {0}'.format(newRightArmPose))
+    #   self.valid = True
+    # else:
+    #   rospy.loginfo('Invalid Right Arm Pose')
+    #   print newRightArmPose
+    #   self.valid = False
+    #   return self
+    # rospy.sleep(.01)
+    # if newLeftJoints is not None:
+    #   self.leftArm.go_to_joints(newLeftJoints)
+    # else:
+    #   rospy.loginfo('Invalid Left Arm Pose')
     rospy.sleep(.01)
-    if newLeftJoints is not None:
-      self.leftArm.go_to_joints(newLeftJoints)
-    else:
-      rospy.loginfo('Invalid Left Arm Pose')
-    rospy.sleep(.01)
-    return self # Modify to generate a valid succesor states, does not update state if pose is invalid and instead reverts to last valid state
+    return self # Modify to generate a valid successor states, does not update state if pose is invalid and instead reverts to last valid state
 
   def alignGrippers(self):
     rightArmPose = self.rightArm.get_pose()
@@ -199,7 +217,7 @@ def main():
 
   while not state.isGoalState():
     print 'Getting Next State...'
-    state = state.succesorState()
+    state = state.successorState()
 
   state.rightArm.open_gripper()
   rospy.sleep(0.5)
